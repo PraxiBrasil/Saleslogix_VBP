4093980e610e9c3041b2e2f32a8549b0b65f8b87
PraxiBrasil <mzanella@praxisol.com>
2011-11-21 23:06:07 -0200

    Zanella - 21/11/2011

---
+++ Model/Portal/SlxClient/SupportFiles/SmartParts/SalesOrder/Embraer.js
@@ -0,0 +1,10 @@
+<script language="javascript" type="text/javascript">
+	function SetDivDisplay(divId, display)
+	{ 
+		var control = document.getElementById(divId);
+		if (control != null)
+		{
+			control.style.display = display;
+		}
+	}
+</script>
